2071dd1ceb92305f67d24d5b5e8738630c7a9199
Aishwarya B <35876177+Aishwarya0206@users.noreply.github.com>
2021-01-20 01:54:29 +0530

    Hello world program

---
+++ hello_world_pro/src/hello_world_pro/hello_world_pro.java
@@ -0,0 +1,10 @@
+package hello_world_pro;
+
+public class hello_world_pro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
